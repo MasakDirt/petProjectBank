5422fc2f03a4de2beae685802718e54901657d57
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-17 12:33:19 +0300

    added dependency, creating Transaction and TransactionHistory classes

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,4 +1,83 @@
 package model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Table
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    private TransactionHistory history;
+    @CreationTimestamp
+    private LocalDateTime doneAt;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "owner_id")
+    private Customer owner;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "customer_send_id")
+    private Customer willSend;
+
+    public Transaction() {
+        doneAt = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public TransactionHistory getHistory() {
+        return history;
+    }
+
+    public LocalDateTime getDoneAt() {
+        return doneAt;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Customer getWillSend() {
+        return willSend;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setHistory(TransactionHistory history) {
+        this.history = history;
+    }
+
+    public void setDoneAt(LocalDateTime createdAt) {
+        this.doneAt = createdAt;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public void setWillSend(Customer willSend) {
+        this.willSend = willSend;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", createdAt=" + doneAt +
+                ", owner=" + owner +
+                '}';
+    }
 }
--- src/main/java/model/TransactionHistory.java
+++ src/main/java/model/TransactionHistory.java
@@ -1,4 +1,42 @@
 package model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Table
+@Entity
 public class TransactionHistory {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @OneToMany
+    @NotNull
+    private List<Transaction> transactions;
+    public TransactionHistory(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionHistory{" +
+                "id=" + id +
+                ", transactions=" + transactions +
+                '}';
+    }
 }
