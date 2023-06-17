ba9be718ee54fced387b69620fbb99c84489b0b3
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-17 12:39:10 +0300

    remove Customers field

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -2,8 +2,10 @@ package model;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Table
@@ -14,16 +16,6 @@ public class Transaction {
     private long id;
     @CreationTimestamp
     private LocalDateTime doneAt;
-
-    @ManyToOne
-    @NotNull
-    @JoinColumn(name = "owner_id")
-    private Customer owner;
-
-    @ManyToOne
-    @NotNull
-    @JoinColumn(name = "customer_send_id")
-    private Customer willSend;
 
     public Transaction() {
         doneAt = LocalDateTime.now();
@@ -37,14 +29,6 @@ public class Transaction {
         return doneAt;
     }
 
-    public Customer getOwner() {
-        return owner;
-    }
-
-    public Customer getWillSend() {
-        return willSend;
-    }
-
     public void setId(long id) {
         this.id = id;
     }
@@ -52,21 +36,11 @@ public class Transaction {
     public void setDoneAt(LocalDateTime createdAt) {
         this.doneAt = createdAt;
     }
-
-    public void setOwner(Customer owner) {
-        this.owner = owner;
-    }
-
-    public void setWillSend(Customer willSend) {
-        this.willSend = willSend;
-    }
-
     @Override
     public String toString() {
         return "Transaction{" +
                 "id=" + id +
                 ", createdAt=" + doneAt +
-                ", owner=" + owner +
                 '}';
     }
 }
