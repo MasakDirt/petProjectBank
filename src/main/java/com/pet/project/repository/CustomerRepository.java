01ce84655e46528636d5044bb1754b5b672394d9
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-17 23:34:27 +0300

    сustomerService as if complete

--- src/main/java/com/pet/project/repository/CustomerRepository.java
+++ src/main/java/com/pet/project/repository/CustomerRepository.java
@@ -4,6 +4,22 @@ import com.pet.project.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Override
+    <S extends Customer> S save(S s);
+
+    @Override
+    void delete(Customer customer);
+
+    Customer findCustomerByEmail(String email);
+
+    @Override
+    Optional<Customer> findById(Long id);
+
+    @Override
+    List<Customer> findAll();
 }
---
+++ src/main/java/com/pet/project/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,70 @@
+package com.pet.project.service.impl;
+
+import com.pet.project.exception.NullEntityReferenceException;
+import com.pet.project.model.Customer;
+import com.pet.project.repository.CustomerRepository;
+import com.pet.project.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+    CustomerRepository customerRepository;
+
+    @Override
+    public Customer create(Customer customer) {
+        try {
+            return customerRepository.save(customer);
+        } catch (IllegalArgumentException exception) {
+            throw new NullEntityReferenceException("Customer cannot be 'null'");
+        }
+    }
+
+    @Override
+    public Customer readById(long id) {
+        Optional<Customer> optional = customerRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        throw new EntityNotFoundException("Customer with id " + id + " not found");
+    }
+
+    @Override
+    public void delete(long id) {
+        Customer customer = readById(id);
+        if (customer != null) {
+            customerRepository.delete(customer);
+        }else {
+            throw new EntityNotFoundException("Customer with id " + id + " not found");
+        }
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        if (customer != null) {
+            Customer oldCustomer = readById(customer.getId());
+            return customerRepository.save(oldCustomer);
+        }
+        throw new NullEntityReferenceException("Customer cannot be 'null'");
+    }
+
+    @Override
+    public Customer findByEmail(String email) {
+        if (email != null) {
+            return customerRepository.findCustomerByEmail(email);
+        }
+        throw new NullEntityReferenceException("Email cannot be 'null'");
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers.isEmpty() ? new ArrayList<>() : customers;
+    }
+}
