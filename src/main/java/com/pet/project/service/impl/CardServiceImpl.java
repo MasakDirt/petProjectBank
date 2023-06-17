bfe4b46ff5fc177c8becacad80427d5f77096adb
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-18 00:08:28 +0300

    added cardServices classes with it`s realizations

---
+++ src/main/java/com/pet/project/service/impl/CardServiceImpl.java
@@ -0,0 +1,89 @@
+package com.pet.project.service.impl;
+
+import com.pet.project.exception.NullEntityReferenceException;
+import com.pet.project.model.Card;
+import com.pet.project.model.Customer;
+import com.pet.project.repository.CardRepository;
+import com.pet.project.service.CardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CardServiceImpl implements CardService {
+    CardRepository cardRepository;
+
+    @Override
+    public Card create(Card card) {
+        try {
+            return cardRepository.save(card);
+        } catch (IllegalArgumentException exception) {
+            throw new NullEntityReferenceException("Card cannot be 'null'");
+        }
+    }
+
+    @Override
+    public void delete(long id) {
+        Card card = readById(id);
+        if (card != null) {
+            cardRepository.delete(card);
+        }
+    }
+
+    @Override
+    public Card update(Card card) {
+        if (card != null) {
+            Card oldCard = readById(card.getId());
+            if (oldCard != null){
+                return cardRepository.save(card);
+            }
+        }
+        throw new NullEntityReferenceException("Card cannot be 'null'");
+    }
+
+    @Override
+    public Card readById(long id) {
+        Optional<Card> optional = cardRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        throw new EntityNotFoundException("Card with id " + id + " not found");
+    }
+
+    @Override
+    public Card readByNumber(String number) {
+        if (number != null) {
+            return cardRepository.findCardByNumber(number);
+        }
+        throw new NullEntityReferenceException("Number cannot be 'null'");
+    }
+
+    @Override
+    public Card readByOwner(Customer owner) {
+        Optional<Card> optional = cardRepository.findCardByOwner(owner);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        throw new NullEntityReferenceException("Owner cannot be null!");
+    }
+
+    @Override
+    public List<Card> getAllByOwner(Customer owner) {
+        if (owner != null) {
+            List<Card> ownersCards = cardRepository.findAllByOwner(owner);
+            return ownersCards.isEmpty() ? new ArrayList<>() : ownersCards;
+        }
+        throw new NullEntityReferenceException("Owner cannot be null!");
+    }
+
+    @Override
+    public List<Card> getAll() {
+        List<Card> cards = cardRepository.findAll();
+        return cards.isEmpty() ? new ArrayList<>() : cards;
+    }
+}
