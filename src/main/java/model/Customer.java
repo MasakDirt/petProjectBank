91ab3e01369657f9133cb75f9943ed95c2063f3c
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-17 10:35:11 +0300

    simply Customer model was done

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,5 +1,103 @@
 package model;
 
-public class Customer {
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
+@Table
+@Entity
+public class Customer {
+    private static final String NAME_REGEXP = "[A-Z][a-z]+(-[A-Z][a-z]+){0,1}";
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Pattern(regexp = NAME_REGEXP,
+            message = "Must start with a capital letter and followed by one or more lowercase")
+    @NotBlank(message = "Your first name cannot be blank")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Pattern(regexp = NAME_REGEXP,
+            message = "Must start with a capital letter and followed by one or more lowercase")
+    @NotBlank(message = "Your last name cannot be blank")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Email(message = "Write a valid e-mail address")
+    @NotBlank
+    @Column(name = "e-mail", nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "card_id")
+    @NotNull
+    private Card card;
+
+    public Customer(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
