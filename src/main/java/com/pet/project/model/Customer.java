eb2103bd715279d096b483dbe1ca05c10179d711
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-17 21:16:54 +0300

    finish customer refactoring and create tests for it

--- src/main/java/com/pet/project/model/Customer.java
+++ src/main/java/com/pet/project/model/Customer.java
@@ -1,8 +1,12 @@
 package com.pet.project.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Table(name = "customer")
@@ -11,26 +15,36 @@ public class Customer {
     private static final String NAME_REGEXP = "[A-Z][a-z]+(-[A-Z][a-z]+){0,1}";
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "customer_sequence"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     private long id;
 
     @Pattern(regexp = NAME_REGEXP,
             message = "Must start with a capital letter and followed by one or more lowercase")
-    @NotBlank(message = "Your first name cannot be blank")
+    @NotNull
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @Pattern(regexp = NAME_REGEXP,
             message = "Must start with a capital letter and followed by one or more lowercase")
-    @NotBlank(message = "Your last name cannot be blank")
+    @NotNull
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Email(message = "Write a valid e-mail address")
     @NotBlank
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @NotBlank(message = "Password cannot be blank!!!")
     @Column(nullable = false)
     private String password;
 
