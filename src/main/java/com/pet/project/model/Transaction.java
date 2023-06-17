5f4f36e38530a1bfd3a13daf22fd9004f4fe09ff
maksym_korniev <maksimkarulet8@gmail.com>
2023-06-17 22:00:58 +0300

    finish transaction refactoring and create tests for it

--- src/main/java/com/pet/project/model/Transaction.java
+++ src/main/java/com/pet/project/model/Transaction.java
@@ -1,6 +1,8 @@
 package com.pet.project.model;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +14,16 @@ import java.time.LocalDateTime;
 @Entity
 public class Transaction {
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "transaction_sequence"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     private long id;
     @CreationTimestamp
     private LocalDateTime doneAt;
