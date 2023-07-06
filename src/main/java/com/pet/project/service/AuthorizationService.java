7200f8d2a161e9b19861927e0c5b4eb20be14fa1
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-06 15:42:15 +0300

    refactor all log.info fields and added UNAUTHORIZED status, added methods for checking user authorization

--- src/main/java/com/pet/project/service/AuthorizationService.java
+++ src/main/java/com/pet/project/service/AuthorizationService.java
@@ -13,6 +13,31 @@ public class AuthorizationService {
         return customerService.readById(id).getEmail().equals(getPrincipal());
     }
 
+    public boolean isUserAdminOrValidUserAndIsCardOwner(long ownerId, long cardId) {
+        return isUserAdminOrIsUsersSame(ownerId) && isCardOwner(ownerId, cardId);
+    }
+
+    public boolean isUserAdminOrIsUsersSame(long id) {
+        return isAdmin(id) || checkIfUsersSame(id);
+    }
+
+    public boolean isUserAdminOrIsUsersSameAndUpdateIdEqual(long id, long updateId) {
+        return isAdmin(id) || (checkIfUsersSame(id) && checkIfUsersSame(updateId));
+    }
+
+    private boolean isCardOwner(long ownerId, long cardId) {
+        return customerService.readById(ownerId).getMyCards()
+                .stream()
+                .anyMatch(card -> card.getId() == cardId);
+    }
+
+    private boolean isAdmin(long id) {
+        if (checkIfUsersSame(id)) {
+            return customerService.readById(id).getRole().getName().equals("ADMIN");
+        }
+        return false;
+    }
+
     private Object getPrincipal() {
         return SecurityContextHolder
                 .getContext()
