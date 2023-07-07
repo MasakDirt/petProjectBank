f1fbb9786ecebd6eb6c26764041e08087e26766a
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-07 21:06:58 +0300

    CardBalanceUpdateRequest: added checking for wrong sum writing.

--- src/main/java/com/pet/project/model/dto/card/CardBalanceUpdateRequest.java
+++ src/main/java/com/pet/project/model/dto/card/CardBalanceUpdateRequest.java
@@ -6,11 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.DecimalMin;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CardBalanceUpdateRequest {
+
     @JsonProperty
+    @DecimalMin(value = "0.1", message = "Sum cannot be less than 0.1")
     private double sum;
+
 }
