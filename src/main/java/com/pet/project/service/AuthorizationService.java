64ceb6c9dbb66fa744755ed9c9f0e54467fe715b
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-08 22:19:40 +0300

    AuthorizationService: reformat methods as usually)

--- src/main/java/com/pet/project/service/AuthorizationService.java
+++ src/main/java/com/pet/project/service/AuthorizationService.java
@@ -1,5 +1,6 @@
 package com.pet.project.service;
 
+import com.pet.project.model.entity.Customer;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,33 +10,33 @@ public class AuthorizationService {
     private final CustomerService customerService;
     private final CardService cardService;
 
-    public boolean isUserAdminOrValidUserAndIsCardOwner(String username, long ownerId, long cardId) {
-        return isAdmin(username) || (isUsersIdEquals(username, ownerId) && isCardOwner(username, cardId));
+    public boolean isUserAdminOrIsUsersSame(String username, long ownerId) {
+        return isAdmin(username) || isUsersIdEquals(username, ownerId);
     }
 
     public boolean isUserValidUserAndIsCardOwner(String username, long ownerId, long cardId) {
         return isUsersIdEquals(username, ownerId) && isCardOwner(username, cardId);
     }
 
-    public boolean isUserAdminOrValidUserAndIsCardOwnerAndIsCardContainsTransaction(String username, long ownerId, long cardId, long transactionId) {
-        return isAdmin(username) || (isUsersIdEquals(username, ownerId) && isCardOwner(username, cardId)
-                && isCardContainsTransaction(cardId, transactionId));
-    }
-
-    public boolean isUserAdminOrIsUsersSame(String username, long ownerId) {
-        return isAdmin(username) || isUsersIdEquals(username, ownerId);
-    }
-
     public boolean isUserAdminOrIsUsersSameForUpdate(String username, long ownerId, long requestId) {
         return (isAdmin(username) || isUsersIdEquals(username, ownerId)) && isUsersIdEquals(ownerId, requestId);
     }
 
+    public boolean isUserAdminOrValidUserAndIsCardOwner(String username, long ownerId, long cardId) {
+        return isAdmin(username) || isUserValidUserAndIsCardOwner(username, ownerId, cardId);
+    }
+
+    public boolean isUserAdminOrValidUserAndIsCardOwnerAndIsCardContainsTransaction(String username, long ownerId, long cardId, long transactionId) {
+        return isAdmin(username) || (isUserValidUserAndIsCardOwner(username, ownerId, cardId)
+                && isCardContainsTransaction(cardId, transactionId));
+    }
+
     public boolean isAdmin(String username) {
-        return customerService.loadUserByUsername(username).getRole().getName().equals("ADMIN");
+        return getCustomer(username).getRole().getName().equals("ADMIN");
     }
 
     private boolean isUsersIdEquals(String currentUsername, long id) {
-        return customerService.loadUserByUsername(currentUsername).getId() == id;
+        return getCustomer(currentUsername).getId() == id;
     }
 
     private boolean isUsersIdEquals(long userId, long requestId) {
@@ -43,14 +44,18 @@ public class AuthorizationService {
     }
 
     private boolean isCardOwner(String username, long cardId) {
-        return customerService.loadUserByUsername(username).getMyCards()
+        return getCustomer(username).getMyCards()
                 .stream()
                 .anyMatch(card -> card.getId() == cardId);
     }
 
     private boolean isCardContainsTransaction(long cardId, long transactionId) {
-        return cardService.readById(cardId).getAccount().getTransactions()
+        return cardService.getHistory(cardId)
                 .stream()
                 .anyMatch(transaction -> transaction.getId() == transactionId);
+    }
+
+    private Customer getCustomer(String username) {
+        return customerService.loadUserByUsername(username);
     }
 }
