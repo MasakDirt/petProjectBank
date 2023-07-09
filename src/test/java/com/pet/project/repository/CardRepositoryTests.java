a74e1952591cc0cff2ee2894b97302d7959cd5e9
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-09 22:13:25 +0300

    CardRepositoryTests, CardServiceImpl, CardServiceTests: updated classes after refactoring AccountServiceImpl for REST controllers

--- src/test/java/com/pet/project/repository/CardRepositoryTests.java
+++ src/test/java/com/pet/project/repository/CardRepositoryTests.java
@@ -11,27 +11,29 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
+import javax.persistence.EntityNotFoundException;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //In Repository layer I check only my methods!
-@ActiveProfiles("test")
-@SpringBootTest
 @Transactional
+@SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 public class CardRepositoryTests {
     private final CustomerRepository customerRepository;
     private final CardRepository cardRepository;
-    private static int idCounter = 9;
+    private final AccountRepository accountRepository;
 
     @Autowired
-    public CardRepositoryTests(CustomerRepository customerRepository, CardRepository cardRepository) {
+    public CardRepositoryTests(CustomerRepository customerRepository, CardRepository cardRepository, AccountRepository accountRepository) {
         this.customerRepository = customerRepository;
         this.cardRepository = cardRepository;
+        this.accountRepository = accountRepository;
     }
+
     @Test
     void injectedComponentsAreNotNull() {
         assertThat(customerRepository).isNotNull();
@@ -42,9 +44,7 @@ public class CardRepositoryTests {
     public void checkFindCardByOwnerAndId() {
         Customer owner = customerRepository.getOne(1L);
 
-        Card expected = createNewCard();
-
-        cardRepository.save(expected);
+        var expected = cardRepository.save(createNewCard());
 
         Card actual = cardRepository.findCardByOwnerAndId(owner, expected.getId())
                 .orElseThrow(() -> new NoSuchElementException("We can not find card with owner " + owner + " and id " + expected.getId()));
@@ -54,8 +54,7 @@ public class CardRepositoryTests {
 
     @Test
     public void checkFindCardByNumber() {
-        Card expected = createNewCard();
-        cardRepository.save(expected);
+        var expected = cardRepository.save(createNewCard());
 
         Card actual = cardRepository.findCardByNumber(expected.getNumber())
                 .orElseThrow(() -> new NoSuchElementException("We can not find card with number: " + expected.getNumber()));
@@ -64,14 +63,13 @@ public class CardRepositoryTests {
     }
 
     private Card createNewCard() {
-        Customer owner = customerRepository.getOne(1L);
+        Customer owner = customerRepository.findById(1L).orElseThrow(() ->
+                new EntityNotFoundException("Customer with id" + 1 + " not found"));
 
-        Account account = new Account();
-        account.setBalance(new BigDecimal(20000));
-        account.setId(20L);
+        Account account = accountRepository.findById(1L).orElseThrow(() ->
+                new EntityNotFoundException("Account with id" + 1 + " not found"));
 
         Card card = new Card();
-        card.setId(idCounter++);
         card.setOwner(owner);
         card.setAccount(account);
 
