f9ee48a03b0791c52efd10d4706d181c63df53fc
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-14 14:56:45 +0300

    TransactionReadResponse class added @JsonDeserialize and @JsonFormat annotations!

--- src/main/java/com/pet/project/model/dto/transaction/TransactionReadResponse.java
+++ src/main/java/com/pet/project/model/dto/transaction/TransactionReadResponse.java
@@ -1,11 +1,13 @@
 package com.pet.project.model.dto.transaction;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
@@ -25,8 +27,9 @@ public class TransactionReadResponse {
     @JsonProperty("funds_withdrawn")
     private BigDecimal fundsWithdrawn;
 
-    @CreationTimestamp
     @JsonProperty("done_at")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime doneAt;
 
     @DecimalMin(value = "0", message = "Account cannot be less than 0")
