66a8a91d9b8224e01eb1e821518e9ae8891b5801
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-14 14:56:30 +0300

    fix bug with unable deserialize LocalDateTime class fields, so in ControllerTestsStaticHelper class I added  module and configure, for TransactionHistoryResponse and TransactionReadResponse classes added @JsonDeserialize and @JsonFormat annotations!

--- src/test/java/com/pet/project/controller/ControllerTestsStaticHelper.java
+++ src/test/java/com/pet/project/controller/ControllerTestsStaticHelper.java
@@ -1,13 +1,16 @@
 package com.pet.project.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.pet.project.model.dto.auth.LoginRequest;
 import com.pet.project.model.dto.customer.CustomerCreateRequest;
 
 public class ControllerTestsStaticHelper {
     public static <T> String asJsonString(final T obj) {
         try {
-            return new ObjectMapper().writeValueAsString(obj);
+            return new ObjectMapper().registerModule(new JavaTimeModule())
+                    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false).writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
