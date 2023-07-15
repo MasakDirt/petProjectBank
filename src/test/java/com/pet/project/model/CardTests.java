ef83e3b66c711ad62c4433d33fbe32cad88a43d1
maksym_korniev <maksimkarulet8@gmail.com>
2023-07-15 18:32:55 +0300

    CardTests: added new test : test_Creating_CardNumber and rename all tests to start with test_...

--- src/test/java/com/pet/project/model/CardTests.java
+++ src/test/java/com/pet/project/model/CardTests.java
@@ -12,12 +12,14 @@ import java.util.Set;
 
 import static com.pet.project.model.ValidatorForTests.getViolations;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 public class CardTests {
     private static Card validCard;
+
     @BeforeAll
-    static void setUp(){
+    static void setUp() {
         validCard = new Card();
         validCard.setId(1);
         validCard.setAccount(new Account());
@@ -25,18 +27,26 @@ public class CardTests {
     }
 
     @Test
-    public void checkValidCard(){
+    public void test_ValidCard() {
         Set<ConstraintViolation<Card>> violations = getViolations(validCard);
         assertEquals(0, violations.size());
     }
 
     @Test
-    public void checkNullOwner(){
+    public void test_NullOwner() {
         Card card = new Card();
         card.setId(2);
         card.setOwner(null);
+        card.setAccount(new Account());
 
         Set<ConstraintViolation<Card>> violations = getViolations(card);
         assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void test_Creating_CardNumber() {
+        Card expected = new Card();
+
+        assertNotEquals(null, expected.getNumber());
     }
 }
